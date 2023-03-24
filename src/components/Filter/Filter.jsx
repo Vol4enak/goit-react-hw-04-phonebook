e4755877f2c7abd3937fb7acdfe5ecb513d8d91a
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-03-24 18:54:35 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 export class Filter extends Component {
-
+ 
     trowValue = (e) => {
         const value = e.currentTarget.value;
         this.props.onChange(value);
     }
   render() {
+     console.log(123);
     return (
       <>
         <p>Find contacts by name</p>
